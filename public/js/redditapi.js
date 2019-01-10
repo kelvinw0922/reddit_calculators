aa2a5c984570066525e4c9a6c6bfca3fd0826fc7
Kelvin <kelvinwong0922@gmail.com>
2019-01-09 16:31:30 -0800

    Uninstall mongoose since it is not being used

--- public/js/redditapi.js
+++ public/js/redditapi.js
@@ -1,6 +1,7 @@
 require("isomorphic-fetch");
 
 module.exports = {
+  // Searching calculator's subreddits
   search: function (subreddit) {
     return fetch(
       `https://www.reddit.com/r/${subreddit}/top/.json?sort=top&t=week&limit=50`
@@ -9,6 +10,7 @@ module.exports = {
       .then(data => data.data.children.map(post_thread => post_thread.data))
       .catch(err => console.log(err));
   },
+  // Searching non-calculator's subreddits (such as Math-related)
   calculator_search: function (subreddit) {
     return fetch(
       `https://www.reddit.com/r/${subreddit}/search.json?q=calculator&sort=new&restrict_sr=on&limit=50&t=month`
